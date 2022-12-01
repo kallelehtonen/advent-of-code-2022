e9f9a7eb99d88b66172edb97fd190dbd5b0e8744
kallelehtonen <coadiespla@gmail.com>
2022-12-01 16:53:51 +0200

    day1

---
+++ src/01.ts
@@ -0,0 +1,23 @@
+import * as fs from "fs";
+
+const input = fs.readFileSync("./input/01.txt", "utf-8").trim();
+const elves = input.split(/\n\s*\n/);
+
+const sum = (a: number, b: number): number => (a + b);
+
+const totalCaloriesByElf = elves.map((elf) => {
+  const calories = elf.trim().split(/\n/).map(Number);
+
+  return calories.reduce(sum, 0);
+}).sort((a, b) => b - a);
+
+const a = (): number => {
+  return totalCaloriesByElf[0];
+};
+
+const b = (): number => {
+  return totalCaloriesByElf.slice(0, 3).reduce(sum, 0);
+};
+
+console.log(a());
+console.log(b());
