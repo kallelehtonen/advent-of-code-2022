d7b6d2a466a73e53be36cb3cc8774ac2c026063d
kallelehtonen <coadiespla@gmail.com>
2022-12-04 11:56:08 +0200

    add template

---
+++ src/template.ts
@@ -0,0 +1,10 @@
+import * as fs from "fs";
+
+const input = fs.readFileSync("./input/template.txt", "utf-8").trim();
+
+const a = () => {};
+
+const b = () => {};
+
+console.log(a());
+console.log(b());
